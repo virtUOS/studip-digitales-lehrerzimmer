8e25f41b49a4641db98b5a6c639a6faa9d950af2
Annelene Sudau <sudau@elan-ev.de>
2019-12-03 13:36:18 +0100

    aenderung übershrift

--- views/pages/study_to_practice.php
+++ views/pages/study_to_practice.php
@@ -1,3 +1,6 @@
+<h1>Vom Studium in die Praxis</h1>
+
+
 <img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/study_to_practice.png"/>
 
 
