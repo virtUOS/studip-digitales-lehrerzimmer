41b803bf50b5fb6272c3010f4549018ce0410580
asudau <sudau@elan-ev.de>
2019-12-12 15:41:13 +0100

    more css

--- views/pages/my_way.php
+++ views/pages/my_way.php
@@ -1,22 +1,28 @@
 
 
+<div class="koop-content">
+
+    <div class="flex-wrapper">
+        
+        <div class="flex-container-half">
+
+            <h1>Fachschaften</h1>
+
+            Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. 
+            Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
+        </div>
+
+        
 
 
-<h1>Fachschaften</h1>
-
-Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
-Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. 
-Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-
-
-
-<ul>
-  <li>Anglistik/Amerikanistik</li>
-  <li>Biologie</li>
-  <li>Chemie</li>
-  <li>Cognitive Science</li>
-</ul>
+        <ul class="flex-container-half-linklist">
+          <li>Anglistik/Amerikanistik</li>
+          <li>Biologie</li>
+          <li>Chemie</li>
+          <li>Cognitive Science</li>
+        </ul>
 
 
 
@@ -30,7 +36,8 @@ Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deseru
   <li class="flex-item"></li>
 </ul>
 
-
+    </div>
+</div>
 
 
 <img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/my_way.png"/>
@@ -38,20 +45,4 @@ Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deseru
 
 <style>
 
-    .flex-container {
-        display: flex;
-    }
-    
-    
-    #page_title_container {
-        display: none;
-    }
-    
-    #flex-header {
-/*        display: none !important;*/
-    }
-
-     #barTopMenu, #barTopStudip {
-        display: none !important;
-    }
 </style>
--- views/pages/study_to_practice.php
+++ views/pages/study_to_practice.php
@@ -1,115 +1,115 @@
 
 <div class="koop-content">
 
-<h1>Vom Studium in die Praxis</h1>
-<div>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
-    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. 
-    Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-</div>
+    <div class="koop-container-full"
+        <h1>Vom Studium in die Praxis</h1>
+        <div>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. 
+            Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </div>
+    </div>
 
-<div class="flex-wrapper">
+    <div class="flex-wrapper">
 
-    <div class="flex-container">
-     <h1>Lehrer_Innen-Persönlichkeit</h1>
-<ul>
-  <li>
-      Präsentation
-  </li>
-  <li style="background-image:url(<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/koop.png'?>)">
-      Stimme und Körpersprache
-  </li>
-  <li>
-      Haltung und Rolle(n)
-  </li>
-  <li>
-      Lehrportfolio
-  </li>
-  <li>
-      Evaluation
-  </li>
-</ul>
-</div>
+        <div class="flex-container-smallbox">
+         <h1>Lehrer_Innen-Persönlichkeit</h1>
+            <ul>
+              <li>
+                  Präsentation
+              </li>
+              <li style="background-image:url(<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/koop.png'?>)">
+                  Stimme und Körpersprache
+              </li>
+              <li>
+                  Haltung und Rolle(n)
+              </li>
+              <li>
+                  Lehrportfolio
+              </li>
+              <li>
+                  Evaluation
+              </li>
+            </ul>
+        </div>
 
-<div class="flex-container">
-<h1>Rund ums Lehramtsstudium</h1>
-<ul>
-  <li>
-      Aktuelle Studien
-  </li>
-  <li>
-      Wissenschaftliches Arbeiten
-  </li>
-  <li>
-      Kommatrainer
-  </li>
-  <li>
-      Recherche
-  </li>
-  <li>
-      Leitfaden Hausarbeit
-  </li>
-  <li>
-      Bedarfsprognosen Lehramt
-  </li>
-</ul>
-</div>
+        <div class="flex-container-smallbox">
+            <h1>Rund ums Lehramtsstudium</h1>
+            <ul>
+              <li>
+                  Aktuelle Studien
+              </li>
+              <li>
+                  Wissenschaftliches Arbeiten
+              </li>
+              <li>
+                  Kommatrainer
+              </li>
+              <li>
+                  Recherche
+              </li>
+              <li>
+                  Leitfaden Hausarbeit
+              </li>
+              <li>
+                  Bedarfsprognosen Lehramt
+              </li>
+            </ul>
+        </div>
 
-<div class="flex-container">
-<h1>Lehrformate und Methoden</h1>
-<ul>
-  <li>
-      Forschendes Lernen
-  </li>
-  <li>
-      Problemorientiertes Lernen
-  </li>
-  <li>
-      Handlungsorientiertes Lernen
-  </li>
-  <li>
-      Phänomenorientiertes Lernen
-  </li>
-  <li>
-      Onterdisziplinäres Lernen
-  </li>
-  <li>
-      Aktivieren und Motivieren
-  </li>
-   <li>
-      Methoden
-  </li>
-</ul>
-</div>
-    
-<div class="flex-container">
-<h1>Unterrichtsplanung und Durchführung</h1>
-<ul>
-  <li>
-      Lehr- und Lenrziele
-  </li>
-  <li>
-      Kompetenzen
-  </li>
-  <li>
-      Feedback
-  </li>
-  <li>
-      Leistungsbeurteilung
-  </li>
-  <li>
-      Didaktische Visualisierung
-  </li>
-  <li>
-      Stundenplanung
-  </li>
-  <li>
-      Unterrichtseinheiten
-  </li>
-</ul>
-</div>    
-    
+        <div class="flex-container-smallbox">
+            <h1>Lehrformate und Methoden</h1>
+            <ul>
+              <li>
+                  Forschendes Lernen
+              </li>
+              <li>
+                  Problemorientiertes Lernen
+              </li>
+              <li>
+                  Handlungsorientiertes Lernen
+              </li>
+              <li>
+                  Phänomenorientiertes Lernen
+              </li>
+              <li>
+                  Interdisziplinäres Lernen
+              </li>
+              <li>
+                  Aktivieren und Motivieren
+              </li>
+               <li>
+                  Methoden
+              </li>
+            </ul>
+        </div>
 
-</div>
+        <div class="flex-container-smallbox">
+            <h1>Unterrichtsplanung und Durchführung</h1>
+            <ul>
+              <li>
+                  Lehr- und Lenrziele
+              </li>
+              <li>
+                  Kompetenzen
+              </li>
+              <li>
+                  Feedback
+              </li>
+              <li>
+                  Leistungsbeurteilung
+              </li>
+              <li>
+                  Didaktische Visualisierung
+              </li>
+              <li>
+                  Stundenplanung
+              </li>
+              <li>
+                  Unterrichtseinheiten
+              </li>
+            </ul>
+        </div>    
+    </div>
 
 </div>
     
@@ -119,7 +119,7 @@
 
 
 <style>
-    .flex-container ul li{
+    .flex-container-smallbox ul li{
         background-image: url(<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/koop.png'?>);
     }
     
