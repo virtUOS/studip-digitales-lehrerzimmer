81934e3a0e398bd4becc5f5def70664c437420b1
asudau <sudau@elan-ev.de>
2019-12-12 13:07:22 +0100

    test

--- views/pages/my_way.php
+++ views/pages/my_way.php
@@ -1,8 +1,33 @@
+
+
+
+
+<h1>Fachschaften</h1>
+
+
+<ul class="flex-container">
+  <li class="flex-item">4</li>
+  <li class="flex-item">5</li>
+</ul>
+
+<ul class="flex-container">
+  <li class="flex-item">1</li>
+  <li class="flex-item">2</li>
+</ul>
+
+
+
+
 <img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/my_way.png"/>
 
 
 <style>
 
+    .flex-container {
+        display: flex; /* or inline-flex */
+    }
+    
+    
     #page_title_container {
         display: none;
     }
