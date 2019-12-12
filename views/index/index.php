46dedd084b2f23051c78720b06de7f48c7ec4657
asudau <sudau@elan-ev.de>
2019-12-12 16:35:03 +0100

    kacheln

--- views/index/index.php
+++ views/index/index.php
@@ -1,4 +1,58 @@
 
+<div class="koop-content">
+
+
+    <div class="flex-wrapper">
+        
+        <!-- Kachelen -->
+        <div class="flex-container-half">
+            
+            <img src='<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/teach_os.png' ?>'/>
+            <!-- Zeile1 -->
+            <div class="flex-wrapper">
+                 <div class='flex-container-200-200'>
+                    <div class="flex-wrapper-td">
+                        <div class='flex-container-200-100'></div>
+                        <div class='flex-container-200-100'></div>
+                     </div>
+                 </div>
+                <div class='flex-container-200-200'></div>
+            </div>
+            
+            <!-- Zeile2 -->
+            <div class="flex-wrapper">
+                <div class='flex-container-200-100'>
+                    <div class="flex-wrapper">
+                        <div class='flex-container-100-100'>
+                            <div class="flex-wrapper-td">
+                                <div class='flex-container-100-50'></div>
+                                <div class='flex-container-100-50'></div>
+                            </div>
+                        </div>
+                        <div class='flex-container-100-100'></div>
+                    </div>
+                </div>
+                <div class='flex-container-200-100'></div>
+            </div>
+            
+            <!-- Zeile3 -->
+            <div class="flex-wrapper">
+                <div class='flex-container-200-100'></div>
+                <div class='flex-container-200-100'>
+                    <div class="flex-wrapper-td">
+                         <div class='flex-container-100-100'></div>
+                         <div class='flex-container-100-100'></div>
+                     </div>
+                </div>
+            </div>
+        </div>
+            
+    </div>
+</div>
+
+
+
+
 
 <img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/index.png"/>
 
