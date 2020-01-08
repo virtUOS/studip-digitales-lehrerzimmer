7b0d0049fd04a41874bf4d78f741792ec48bc9eb
Annelene Sudau <sudau@elan-ev.de>
2020-01-08 13:02:43 +0100

    content for Studienverlaufspläne

--- views/pages/studyplan.php
+++ views/pages/studyplan.php
@@ -1,1 +1,12 @@
 
+<div class="koop-content">
+
+    <div class="koop-container-full">
+        <h1>Studienverlaufspläne</h1>
+        <hr class="header-border-bottom" align="left" />
+        <div>Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. 
+            Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        </div>
+    </div
+</div>
