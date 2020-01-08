5689cf7ef975e23d0556a71b2dd84f093c97a5aa
Annelene Sudau <sudau@elan-ev.de>
2020-01-08 13:26:31 +0100

    inhalt

--- views/pages/consulting.php
+++ views/pages/consulting.php
@@ -1,1 +1,14 @@
 
+<div class="koop-content">
+
+    <div class="flex-wrapper">
+        
+        <div class="flex-container-half">
+
+            <h1>Fachschaften</h1>
+
+            Auf Fachbereichsebene werden die Studierenden durvh die von ihnen gewählten Fachschaften vertreten. Der Fachschaftsrat gilt als Vertretung der
+            Studierenden eines Fachbereiches. Neben der Vertretungsfunktion erfüllen die Mitglieder der Fachschaften sehr unterschiedliche Aufgaben.
+        </div>
+  </div>
+</div>
