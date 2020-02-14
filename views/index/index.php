6e148020aadadcd95b7fbc73c3275672c2343b88
asudau <sudau@elan-ev.de>
2020-02-14 13:16:57 +0100

    more style

--- views/index/index.php
+++ views/index/index.php
@@ -101,7 +101,7 @@
 
 
 
-<img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/index.png"/>
+<img style="margin-left: auto; margin-right: auto; display: block;" src="<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() ?>/assets/images/manual.png"/>
 
 <style>
     #layout-sidebar {
