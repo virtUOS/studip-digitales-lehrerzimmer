fd091de57045a19bfb1cb95d85f0ea393804d062
jhogh <58301962+jhogh@users.noreply.github.com>
2020-03-01 08:08:05 +0100

    fix

--- views/index/index.php
+++ views/index/index.php
@@ -8,7 +8,7 @@
         <div class="flex-container-half">
             
 -->            
-    <img id='teachuos-logo' style='width:350px' src='<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/0_teachUOS_a.svg' ?>'/>
+    <img id='teachuos-logo' style='width:350px' src='<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/0_teachUOS.svg' ?>'/>
 <!--
              Zeile1 
             <div class="flex-wrapper">
