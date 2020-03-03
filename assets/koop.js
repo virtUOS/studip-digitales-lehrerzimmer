9cc341eb6b1a3ceb29b4c7df6fadb055238b1473
Annelene Sudau <sudau@elan-ev.de>
2020-03-03 16:52:07 +0100

    fix url

--- assets/koop.js
+++ assets/koop.js
@@ -1,3 +1,3 @@
 $( document ).ready(function() {
-    $('.cw-sidebar').before('<a href="plugins.php/koop/index"><div id="koop-sidebar"></div></a>');
+    $('.cw-sidebar').before('<a href="/studip/qplus-koop/plugins.php/koop/index"><div id="koop-sidebar"></div></a>');
 });
