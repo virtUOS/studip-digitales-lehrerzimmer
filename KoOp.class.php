ccbdd745ebebeebf3718fb4895105fb4006e066b
Annelene Sudau <sudau@elan-ev.de>
2020-03-03 15:55:17 +0100

    fix

--- KoOp.class.php
+++ KoOp.class.php
@@ -117,13 +117,13 @@ class KoOp extends StudipPlugin implements SystemPlugin
             
             Navigation::addItem('/koop', $navigation);  
         }    
+        if (Course::findCurrent()->id == 'a4204c684df5c46ec74a6f6420f1d81d'){
+            PageLayout::addStylesheet($this->getPluginURL().'/assets/koop_sem.css');
+        }
     }
 
     public function initialize ()
     {
-        if (true || Course::findCurrent()->id == 'a4204c684df5c46ec74a6f6420f1d81d'){
-            PageLayout::addStylesheet($this->getPluginURL().'/assets/koop_sem.css');
-        }
     }
 
     public function perform($unconsumed_path)
