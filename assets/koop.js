dec08bd3982d537e09591c6a63c3572fc26b18b4
Annelene Sudau <sudau@elan-ev.de>
2020-03-12 15:39:03 +0100

    wrong filename

--- assets/koop.js
+++ assets/koop.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
     
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", 'https://studip3g-test.rz.uni-osnabrueck.de/studip/qplus-koop/plugins_packages/virtUOS/KoOp/menu.html', false);
+    xmlhttp.open("GET", 'https://studip3g-test.rz.uni-osnabrueck.de/studip/qplus-koop/plugins_packages/virtUOS/KoOp/demo.html', false);
     xmlhttp.send();
     var text = xmlhttp.responseText;
     console.log($('.cw-sidebar'));
