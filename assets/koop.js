ef1dd800e053879667bb921222a77cf9de1b170d
Annelene Sudau <sudau@elan-ev.de>
2020-03-12 15:37:47 +0100

    try again

--- assets/koop.js
+++ assets/koop.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
+    
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open("GET", 'https://studip3g-test.rz.uni-osnabrueck.de/studip/qplus-koop/plugins_packages/virtUOS/KoOp/menu.html', false);
+    xmlhttp.send();
+    var text = xmlhttp.responseText;
     console.log($('.cw-sidebar'));
-    $('.cw-sidebar').load('../demo.html');
+     $('.cw-sidebar').before(text);
 });
 
