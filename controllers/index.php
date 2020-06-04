5fa795ec0454764c2db81246f4c7777996e2bef3
Firas <fiadleh@uos.de>
2020-06-04 10:12:16 +0200

    require_once dir fix

--- controllers/index.php
+++ controllers/index.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__file__).'\..\models\koop_page.php';
+require_once dirname(__file__).'/../models/koop_page.php';
 
 class IndexController extends StudipController {
     
--- controllers/pages.php
+++ controllers/pages.php
@@ -1,5 +1,5 @@
 <?php
-require_once dirname(__file__).'\..\models\koop_page.php';
+require_once dirname(__file__).'/../models/koop_page.php';
 
 class PagesController extends StudipController {
     
