cb36401c1d87f1f6c6399ec7f8281392caa03ef6
Firas <fiadleh@uos.de>
2020-07-22 21:30:42 +0200

    fix homepage + sidemenu

--- templates/koop_page_2.php
+++ templates/koop_page_2.php
@@ -4,8 +4,18 @@
      <a class='koop-back'  href='..\'>
      ZURÜCK
      </a>    
+     
+     <div class="koop-footer22 light">
+            
+         	<img width="90"  src="<?=$ABSOLUTE_URI_STUDIP ?><?= $getPluginPath ?>/assets/images2/B0_teachUOS1.svg" /><br>
+         	VOM ZENTRUM FÜR LEHRERBILDUNG DER<br>
+         	<p class="black fontsize13">
+         		UNVERSITÄT OSNABRÜCK
+         	</p>
+           
+         </div>  
    </div>
    <div class='koop-text-behalter'>
-            
+      
    </div>
 </div>
--- views/index/index.php
+++ views/index/index.php
@@ -1,4 +1,6 @@
 <? if (UserConfig::get($GLOBALS['user']->id)->koop_layout == 1) : ?>
+	<img align="right" style="cursor: pointer;margin: 30px;background-color: #3c68ac;" onclick="$( '#set_layout_form' ).submit();" width="110"  src="<?=$GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images2/A0_switch.svg' ?>" />
+        
     <div class="koop-content-centered koop-content">
     
     
@@ -8,6 +10,7 @@
             <div class="flex-container-half">
                 
     -->            
+    		
         <img id='teachuos-logo' style='width:350px' src='<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images/0_teachUOS.svg' ?>'/>
     <!--
                  Zeile1 
@@ -274,6 +277,9 @@
     	</div>
     	
     </div>
+    <form id='set_layout_form'  method="post" action="index/set_layout">
+    				<input type="hidden" name="koop_layout" value="2">
+    </form>
    	<style>
 
     
@@ -437,9 +443,10 @@
 <? else: ?>
 	<div class="koop-content-centered2 ">
 		<div class=koop-content2>
-		    <div >
+			<img align="right" style="cursor: pointer;margin: 30px;" onclick="$( '#set_layout_form' ).submit();" width="110"  src="<?=$GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images2/A0_switch.svg' ?>" />
+        	
+		    <div class="teachuos_logo">
         		<img id='teachuos-logo2' style='width:290px' src='<?= $GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images3/A_teachUOS.svg' ?>'/>
-        		<img align="right" style="cursor: pointer;" onclick="$( '#set_layout_form' ).submit();" width="110"  src="<?=$GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images2/A0_switch.svg' ?>" />
         	</div>
         	<div class="flex-container-1000-200-l100">	
         	<!--  
@@ -504,25 +511,28 @@
             </div>
          </div>   
          <div class="koop-footer2">
+         	<div class="koop-footer-line"></div>
+         	<div class="koop-footer-content">
          	<h3>Herzlich Willkommen auf teachUOS!</h3>
-         	<p class="light">
-         		Du suchst nach Orientierung in deinem Lehramtsstudium? Wer kann dir helfen und wo findest du die richtigen Informationen? Du möchtest deinen eigenen Lernprozess durch Lerntools oder -videos unterstützen? Du interessierst dich über das Studienangebot hinaus für Möglichkeiten des digitalen Unterrichts? Vielleicht hast du auch Lust, einen Blick in andere Fächer zu werfen oder denkst über einen Auslandsaufenthalt nach? Viele Anregungen, Hinweise und Tipps findest du hier auf <font color="#7F9CE3" >teachUOS</font>!
-         	</p>
-         	<p class="light">
-         		<font color="#7F9CE3" >teachUOS</font> ist fächerübergreifend angelegt und richtet sich an alle Lehramtsstudierende. Unimitarbeitende aus dem wissenschaftlichen und organisatorischen Bereich sowie Studierende haben an der Entwicklung dieses Portals mitgewirkt. <font color="#7F9CE3" >teachUOS</font> lädt zum Stöbern und Mitgestalten ein. Melde dich gerne hier, falls du Infos vermisst oder Tipps für Verbesserungen hast! 
-			</p>
+             	<p class="light">
+             		Du suchst nach Orientierung in deinem Lehramtsstudium? Wer kann dir helfen und wo findest du die richtigen Informationen? Du möchtest deinen eigenen Lernprozess durch Lerntools oder -videos unterstützen? Du interessierst dich über das Studienangebot hinaus für Möglichkeiten des digitalen Unterrichts? Vielleicht hast du auch Lust, einen Blick in andere Fächer zu werfen oder denkst über einen Auslandsaufenthalt nach? Viele Anregungen, Hinweise und Tipps findest du hier auf <font color="#7F9CE3" >teachUOS</font>!
+             	</p>
+             	<p class="light">
+             		<font color="#7F9CE3" >teachUOS</font> ist fächerübergreifend angelegt und richtet sich an alle Lehramtsstudierende. Unimitarbeitende aus dem wissenschaftlichen und organisatorischen Bereich sowie Studierende haben an der Entwicklung dieses Portals mitgewirkt. <font color="#7F9CE3" >teachUOS</font> lädt zum Stöbern und Mitgestalten ein. Melde dich gerne hier, falls du Infos vermisst oder Tipps für Verbesserungen hast! 
+    			</p>
+			</div>
          </div>  
-         
+         <!-- 
          <div class="koop-footer22 light">
             <img width="110"  src="<?=$GLOBALS['ABSOLUTE_URI_STUDIP'] . $this->plugin->getPluginPath() . '/assets/images2/A0_quali.svg' ?>" />
-         <!-- 
+         
          	VOM ZENTRUM FÜR LEHRERBILDUNG DER
          	<p class="black fontsize13">
          		UNVERSITÄT OSNABRÜCK
          	</p>
-           -->
+           
          </div>  
-      
+      -->
     </div>
     <form id='set_layout_form'  method="post" action="index/set_layout">
     				<input type="hidden" name="koop_layout" value="1">
@@ -543,20 +553,31 @@
     #layout_container{
     	padding:0px !important;
     }
+    #layout_content{
+    	min-width: 1300px !important;
+    }
+    
+    
+    .teachuos_logo {
+    	font-size: 13px;
+    	width: 1200px;
+    	margin-left: auto;
+    	margin-right: auto;
+    }
     
     #teachuos-logo2{
         margin-top: 105px;
-        margin-left: 490px;
+        margin-left: 132px;
         margin-bottom: 0px;
     }
     
     
     .flex-container-1000-200-l100 {
-        width: 70%;
+        /*width: 70%;*/
         height: 150px;
         /*margin-left: 15%;*/
         font-size: 13px;
-        max-width: 1200px;
+        width: 1200px;
         margin-left: auto;
         margin-right: auto;
     }
@@ -565,7 +586,8 @@
         float:left;
         /*text-align: center;*/
         color: #ffffff;
-        width: 25%;
+        /*width: 25%;*/
+        width: 300px;
         height: 100px;
     }
     /*
@@ -588,29 +610,31 @@
     }
     
     .index_comic1 p{
-        margin-left: 6.8%;
+        margin-left: 130px;
+        width: max-content;
     }
     .index_comic2 p{
-        margin-left: 5%;
+        margin-left: 100px;
+        width: max-content;
     }
     .index_comic3 p{
-        margin-left: 7.0%;
+        margin-left: 135px;
+        width: max-content;
     }
     .index_comic4 p{
-        margin-left: 5.8%;
+        margin-left: 120px;
+        width: max-content;
     }
 
     
     .index_comic1 img{
         width:230px;
         margin-top: 24px;
-        margin-left: 3.3%;
-        /* margin-left: 5%;*/
+        margin-left: 65px;
+        /* margin-left: 3.3%;*/
         margin-right: auto;
         /*margin-left: 30%;
         */
-        
-        
     }
     
     
@@ -619,9 +643,9 @@
     .index_comic2 img{
         width:230px;
         margin-top: 6px;
-        margin-left: 2.9%;
+        margin-left: 52px;
         /*margin-top: 62px;
-        margin-left: 5%;*/
+        margin-left: 2.9%;*/
        margin-right: auto;
        
         /*
@@ -634,9 +658,9 @@
     .index_comic3 img{
         width:230px;
         margin-top: 43px;
-        margin-left: 3.4%;
+        margin-left: 65px;
         /*margin-top: 91px;
-         margin-left: 5%;*/
+         margin-left: 3.4%;*/
         margin-right: auto;
         /*
         margin-left: 10%;
@@ -649,9 +673,9 @@
     .index_comic4 img{
         width:245px;
         margin-top: 35px;
-        margin-left: 3.8%;
+        margin-left: 75px;
         /*margin-top: 83px;
-        /* margin-left: 5%;*/
+        /* margin-left: 3.8%;*/
         margin-right: auto;
         /*
         margin-left: 10%;
@@ -676,22 +700,26 @@
         width: 100%;
         height: 100%;
         background-color: #ffffff;
+        min-width: 1300px;
     }
     
     .koop-content2{
         width: 100%;
         font-size: 13px;
         background-color: #28497c;
-
+        min-width: 1300px;
     }
     .koop-footer2{
         font-size: 15px;
-        width: 56.5%;
+        /*width: 56.5%;*/
+        width: 1170px;
         margin-top: 105px;
-        margin-left: 24%;
+        /*margin-left: 24%;*/
+        margin-left: auto;
+        margin-right: auto;
         color: black;
-        border-left: 1.5px solid #28497c;
-        padding-left: 2%;
+        /*border-left: 1.5px solid #28497c;
+        /*padding-left: 2%;*/
         padding-bottom: 15px;
     }
     .koop-footer2 h3{
@@ -699,6 +727,18 @@
         color: #4F6EB9;
         font-weight: normal;
         padding-top: 15px;
+        margin-left: 40px;
+    }
+    .koop-footer2 p{
+        margin-left: 40px;
+    }
+    
+    .koop-footer-line{
+        border-left: 1.5px solid #28497c;
+        height: 230px;
+        float: left;
+        width: 40px;
+	    margin-left: 80px;
     }
     
     .koop-footer22{
@@ -723,21 +763,7 @@
     }
     
     
-    .light {
-      font-weight: 300;
-    }
-    .regular {
-      font-weight: 400;
-    }
-    .bold {
-      font-weight: 700;
-    }
-    .black {
-      font-weight: 900;
-    }
-    .fontsize13 {
-      font-size: 13px;
-    }
+    
     </style>
 <? endif ?>
 
